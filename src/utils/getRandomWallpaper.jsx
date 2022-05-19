3ef902402db018627e6d6b532ea020e61d460f71
Raj Bhinde <68704186+irajbhinde@users.noreply.github.com>
2022-05-20 02:47:42 +0530

    style : adding white color to app css

--- src/utils/getRandomWallpaper.jsx
+++ src/utils/getRandomWallpaper.jsx
@@ -1,6 +1,5 @@
 import { wallpaperList } from "../assets/BackgroundImages/wallpaper";
 
 const getRandomWallpaper = Math.floor(Math.random(0, wallpaperList.images.length) * 10);
-console.log(getRandomWallpaper);
 
 export { getRandomWallpaper };
